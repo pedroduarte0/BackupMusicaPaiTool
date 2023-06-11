0d540c7e743dc3c6998f30c1b19d2d26928aec38
pedroduarte0 <63309559+pedroduarte0@users.noreply.github.com>
2023-06-11 23:01:57 +0100

    Converting to mp4 and setting converted file timestamp as input file.

--- BackupMusicaPaiTool/Program.cs
+++ BackupMusicaPaiTool/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.IO;
+using System.Diagnostics;
 
 namespace BackupMusicaPaiTool
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {            
             // If no arguments are passed, show message and exit
             if (args.Length == 0)
             {
@@ -24,7 +25,7 @@ namespace BackupMusicaPaiTool
             }
 
             // Get all files from path with search pattern
-            string[] files = Directory.GetFiles(path, "*.wav", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(path, "*.wav", SearchOption.TopDirectoryOnly);
 
             // For each file, memorize the last write time to a variable and restore it.
             foreach (string file in files)
@@ -33,19 +34,46 @@ namespace BackupMusicaPaiTool
                 FileInfo fileInfo = new(file);
                 DateTime lastWriteTime = fileInfo.LastWriteTime;
 
-                Console.WriteLine($"File: {file} - Last write time: {lastWriteTime}");
+                // Convert to mp4
+                string convertedFile = ConvertToMp4(fileInfo, "/home/pedro/Temp/audioconversions");
 
-                // Convert to mp4 here
-                // TODO 
-
-// "/usr/bin/ffmpeg"  -loglevel warning -stats -hide_banner -nostdin -i "/home/pedro/Downloads/pc pai ruido.wav" -vn -sn -map 0:a:? -c:a: alac -map_metadata 0  -threads 4 -y "/home/pedro/Temp/audioconversions/pc pai ruido.m4a"
-// also  https://stackoverflow.com/questions/54456493/ffmpeg-keep-original-file-date
+                // also  https://stackoverflow.com/questions/54456493/ffmpeg-keep-original-file-date
                 // Display converted file name
                 // TODO 
 
                 // Set file last write time to today
-                File.SetLastWriteTime(file, lastWriteTime);
+                File.SetLastWriteTime(convertedFile, lastWriteTime);
             }
+        }
+        
+        private static string ConvertToMp4(FileInfo inputFileInfo, string outputFolder)
+        {
+            // Prepare the process to run
+            ProcessStartInfo start = new();
+
+            string outputFile = Path.Combine(outputFolder,
+                Path.ChangeExtension(inputFileInfo.Name, ".m4a"));// Path.GetFileNameWithoutExtension(inputFileInfo.Name) + ".m4a";
+
+            string arguments = $@"-loglevel warning -stats -hide_banner -nostdin -i ""{inputFileInfo.FullName}"" -vn -sn -map 0:a:? -c:a: alac -map_metadata 0  -threads 4 -y ""{outputFile}""";
+
+            // Enter in the command line arguments, everything you would enter after the executable name itself
+            start.Arguments = arguments; 
+            // Enter the executable to run, including the complete path
+            start.FileName = "/usr/bin/ffmpeg";
+            // Do you want to show a console window?
+            start.WindowStyle = ProcessWindowStyle.Normal;
+            start.CreateNoWindow = true;
+            int exitCode;
+
+            // Run the external process & wait for it to finish
+            using (Process proc = Process.Start(start))
+            {
+                proc.WaitForExit();
+
+                // Retrieve the app's exit code
+                exitCode = proc.ExitCode;
+            }
+            return outputFile;
         }
     }
 }
